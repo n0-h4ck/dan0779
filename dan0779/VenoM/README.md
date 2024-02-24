cd331bb2c57569c67b69a5a7bc185ff6bbef82b0
n0-h4ck <161101458+n0-h4ck@users.noreply.github.com>
2024-02-24 15:21:18 -0300

    dan0779
    biblioteca personalizada de ferramentas para hacking :)

---
+++ dan0779/VenoM/README.md
@@ -0,0 +1,27 @@
+# VenoM
+Herramienta para generar virus programada en python
+# Instalacion
+```
+apt install git -y
+```
+```
+git clone https://github.com/Euronymou5/VenoM.git
+```
+```
+cd VenoM
+```
+```
+chmod +x requirements.sh
+```
+```
+Termux: bash requirements.sh   /   Linux: sudo bash requirements.sh
+```
+```
+python3 venom.py
+```
+
+# Generacion de virus:
+Linux: Scripts en shell / Android: Apk / Windows: bat y exe
+
+# Contacto
+Discord: Negro100pesos#4439
---
+++ dan0779/dan0779.py
@@ -0,0 +1,89 @@
+from time import sleep
+import os, subprocess
+
+
+def limpar_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+import time
+import random
+
+def falar():
+    textos = [
+        "— Boas vindas, Daniel!\n— Inicializando sua ferramenta, aguarde...",
+        "— Salve, mano!\n— Estou executando sua ferramenta agora, bro :)",
+        "— Estava esperando por você!\n— Só um minuto, já iniciando sua ferramenta...",
+        "— Fala aí!\n— Estou preparando sua ferramenta, só um momento...",
+        "— E aí, tudo certo?\n— Estou iniciando sua ferramenta, aguarde um instante...",
+        "— Que bom vê-lo por aqui!\n— Estou configurando sua ferramenta para você...",
+        "— Saudações!\n— Estou prestes a iniciar sua ferramenta, um momento por favor...",
+        "— Oi, como vai?\n— Sua ferramenta está quase pronta para uso...",
+        "— E aí, beleza?\n— Estou preparando tudo para você começar a usar sua ferramenta...",
+        "— Olá!\n— A inicialização da sua ferramenta está em andamento, por favor, aguarde um momento...",
+        "— Opa, chegou!\n— Estou prestes a iniciar sua ferramenta, apenas mais alguns segundos...",
+        "— Hey!\n— Estou configurando tudo para você, sua ferramenta estará pronta em breve..."
+    ]
+      
+    velocidade = 0.1
+    
+    texto_aleatorio = random.choice(textos)
+    
+    for caractere in texto_aleatorio:
+        print(caractere, end='', flush=True)
+        sleep(velocidade)
+
+
+def menu():
+    limpar_console()
+
+    print('''\033[34m
+  ____                     ___    _____   _____    ___  
+ |  _ \    __ _   _ __    / _ \  |___  | |___  |  / _ \ 
+ | | | |  / _` | | '_ \  | | | |    / /     / /  | (_) |
+ | |_| | | (_| | | | | | | |_| |   / /     / /    \__, |
+ |____/   \__,_| |_| |_|  \___/   /_/     /_/       /_/ 
+                                        \033[33mTool do HACKER.
+    \033[m''')
+    print('''
+\033[34mOpções de ferramentas: 
+
+\033[33m[1].\033[34m VenoM \033[90m(criador de vírus)
+\033[33m[2].\033[34m Pyphisher \033[90m(ferramenta de IP logger)
+\033[33m[3].\033[34m Ajuda \033[90m(para quando você precisar)         
+    \033[m''')
+
+    c = input("\033[33mDigite sua opção: \033[m")
+
+    if c == '1':
+        limpar_console()
+        os.chdir("VenoM")
+        subprocess.run(["python", "venom.py"])
+
+    elif c == '2':
+        limpar_console()
+        subprocess.run(["python", "pyphisher.py"])
+    
+    elif c == '3':
+        limpar_console()
+        falar(texto="\033[33mDesculpe, cara, estou desenvolvendo isso ainda.", velocidade=0.1)
+        menu()
+        
+
+    else:
+        limpar_console()
+        falar(texto="\033[33mEssa opção não existe, bro :(", velocidade=0.1)
+        sleep(1)
+        menu()
+
+limpar_console()
+print(''' \033[33m
+─────█─▄▀█──█▀▄─█─────
+────▐▌──────────▐▌────
+────█▌▀▄──▄▄──▄▀▐█────
+───▐██──▀▀──▀▀──██▌───
+──▄████▄──▐▌──▄████▄──
+      ''')
+falar()
+sleep(3)
+menu()
